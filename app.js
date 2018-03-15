f40b6569b24071269f7db218b2cde704ae9d6531
Yanhan Wen <wenyh01@gmail.com>
2018-03-15 00:40:25 -0400

    make app running and make /login/:userid return user info

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const app = express();
+const MongoClient = require('mongodb').MongoClient
+const url = "mongodb://csc309f:csc309fall@ds117316.mlab.com:17316/csc309db"
 
-app.get('/hello', function(req, res){
-  res.send("Hello World")
+MongoClient.connect(url, function(err,res){
+  if(err) console.log(err)
+  console.log("Database created");
+  const db = res.db('csc309db')
+
+  app.get('/hello', function(req, res){
+    res.send("Hello World")
+  })
+
+  app.get('/login/:userId', function(req, res){
+    console.log("userId trying to login: " + req.params.userId)
+    db.collection('coolname-stocks').findOne({user: req.params.userId}).then(function(document){
+      if (document){
+        res.send(document)
+      }
+    })
+  })
+
+  app.listen(3000)
 })
-
-app.listen(3000)
