4e809d3925f7e46e3823b07cfa48f0953b0748b6
Yanhan Wen <wenyh01@gmail.com>
2018-03-17 23:02:50 -0400

    add dbHelper to manipulate database. Should delete after

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
 const MongoClient = require('mongodb').MongoClient
 const url = "mongodb://csc309f:csc309fall@ds117316.mlab.com:17316/csc309db"
+
+app.use(bodyParser.json())
 
 MongoClient.connect(url, function(err,res){
   if(err) console.log(err)
   console.log("Database created");
   const db = res.db('csc309db')
 
+  //test
   app.get('/hello', function(req, res){
     res.send("Hello World")
   })
 
+  //based on userId, retrieve information of this user from database
   app.get('/login/:userId', function(req, res){
     console.log("userId trying to login: " + req.params.userId)
     db.collection('coolname-stocks').findOne({user: req.params.userId}).then(function(document){
@@ -19,6 +24,13 @@ MongoClient.connect(url, function(err,res){
         res.send(document)
       }
     })
+  })
+
+  //format of post: {"userId": "", "stock": "", amount: ""}
+  app.post('/buy', function(req, res){
+    var input = req.body
+    console.log(input)
+    //res.send("What is posted: " + req.body.nima)
   })
 
   app.listen(3000)
---
+++ dbHelper.js
@@ -0,0 +1,49 @@
+const MongoClient = require('mongodb').MongoClient
+const url = "mongodb://csc309f:csc309fall@ds117316.mlab.com:17316/csc309db"
+
+MongoClient.connect(url, function(err,res){
+	if(err) console.log(err)
+	console.log("Database created");
+	const db = res.db('csc309db')
+
+	// Add functions here
+
+  //data to be inserted
+  //this below is inserted
+  //var data = {user:"asdf", password:"123456", stocks:[{symbol: "AAPL", quantity: 1, avgPrice: 100}, {symbol:"FB", quantity: 1, avgPrice: 100}]}
+
+
+  //insert one entry into database
+  // db.collection("coolname-stocks").insertOne(data, function(err, res){
+  //   console.log("INSERT ONE")
+  //
+  //   check all data in database
+  //   db.collection('coolname-stocks').find({}).forEach(function(doc) {
+  //     console.log(doc)
+  //   }, (err) => {
+  //     console.log(err);
+  //   })
+  //
+  //
+  // })
+
+  //   check all data in database
+    db.collection('coolname-stocks').find({}).forEach(function(doc) {
+      console.log(doc)
+    }, (err) => {
+      console.log(err);
+    })
+
+
+  // //check specific entry in Database
+  // db.collection('coolname-stocks').findOne({user: "sb"}).then(function(document){
+  //   if (document){
+  //     console.log(document)
+  //   }
+  // })
+
+  //delete all entry in Database
+  //db. collection('coolname-stocks').deleteMany({})
+
+
+})
